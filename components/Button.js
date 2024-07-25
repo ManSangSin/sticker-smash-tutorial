d506b947fc7a727860b6a2d289e587da979b0a89
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-07-25 01:25:22 +0100

    create button component

---
+++ components/Button.js
@@ -0,0 +1,14 @@
+import { StyleSheet, View, Pressable, Text } from "react-native";
+
+export default function Button({ label }) {
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={styles.button}
+        onPress={() => alert("You pressed a button.")}
+      >
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
