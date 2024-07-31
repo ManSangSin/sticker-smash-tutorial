dd5b62edbb1a87dca67878b930d19231af752b57
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-07-31 23:42:05 +0100

    create useState to store image selected locally

--- App.js
+++ App.js
@@ -3,18 +3,21 @@ import { StyleSheet, Text, View, Image } from "react-native";
 import ImageViewer from "./components/ImageViewer";
 import Button from "./components/Button";
 import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
       quality: 1,
     });
 
-    if (result.canceled) {
-      console.log(result);
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
     } else {
       alert("You did not select an image.");
     }
