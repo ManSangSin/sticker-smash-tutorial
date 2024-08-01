ab1a7d3412632f269d5b0f42a31cc5449c826fe9
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-08-01 16:50:30 +0100

    create state for showAppOptions

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -18,6 +19,7 @@ export default function App() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert("You did not select an image.");
     }
