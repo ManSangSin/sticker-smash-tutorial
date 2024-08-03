17cea41f4380ac7165c49ecabe788a987843863c
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-08-03 23:28:03 +0100

    create IconButton component
    includes props icon, label and onPress

---
+++ components/IconButton.js
@@ -0,0 +1,11 @@
+import { Pressable, Text } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons/MaterialIcons";
+
+export default function IconButton({ icon, label, onPress }) {
+  return (
+    <Pressable onPress={onPress}>
+      <MaterialIcons name={icon} size={24} color="#fff" />
+      <Text>{label}</Text>
+    </Pressable>
+  );
+}
