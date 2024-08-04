086372886b1895166b6a96e1ac206fc44bc6fd85
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-08-04 19:07:44 +0100

    create styles for IconButton and IconLabel

--- components/IconButton.js
+++ components/IconButton.js
@@ -1,11 +1,22 @@
-import { Pressable, Text } from "react-native";
+import { Pressable, StyleSheet, Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons/MaterialIcons";
 
 export default function IconButton({ icon, label, onPress }) {
   return (
-    <Pressable onPress={onPress}>
+    <Pressable styles={styles.iconButton} onPress={onPress}>
       <MaterialIcons name={icon} size={24} color="#fff" />
-      <Text>{label}</Text>
+      <Text styles={styles.iconButtonLabel}>{label}</Text>
     </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  iconButton: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconButtonLabel: {
+    color: "#fff",
+    marginTop: 12,
+  },
+});
